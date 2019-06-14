7b91f7b9b3ec130fdd2c7c2360f246a1bb8d96e8
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-14 18:45:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -15,7 +15,7 @@ def not_safe?(speed)
     return true 
   elsif speed < 40
     return true
-  !!(speed >= 40 && speed =< 60) ? false : true
+  !!(speed >= 40 && speed <= 60) ? false : true
   end
 	
 end
